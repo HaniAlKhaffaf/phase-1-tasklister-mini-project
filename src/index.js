20acca09378d91f64729108b359a7c64fe1ec55d
Hani Amer <cvba2222@gmail.com>
2022-12-16 16:44:38 +0300

    assignment completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector("form")
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    createToDo(e.target.new_task_description.value);
+    form.reset();
+  })
 });
+
+function createToDo(todo) {
+  let li = document.createElement("li")
+  let btn = document.createElement("button");
+  btn.addEventListener("click", removeToDo);
+  btn.textContent = "X";
+  li.textContent = `${todo} `;
+  li.appendChild(btn)
+  document.getElementById("tasks").appendChild(li);
+}
+
+function removeToDo (e) {
+  e.target.parentNode.remove();
+}
